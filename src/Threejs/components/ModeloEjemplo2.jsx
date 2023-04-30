118c07d35b013b37a8177b4a9c1eb0380603fc45
kervis1993 <kervisvasquez24@gmail.com>
2023-04-30 16:08:15 -0400

    load textura to project

--- src/Threejs/components/ModeloEjemplo2.jsx
+++ src/Threejs/components/ModeloEjemplo2.jsx
@@ -15,54 +15,71 @@ const ModeloEjemplo2 = () => {
             1000
         );
 
-        camere.position.z = 6;
+        camere.position.z = 10;
         scene.add(camere);
         const renderer = new THREE.WebGLRenderer();
 
         renderer.setSize(width, height);
         currentMount.appendChild(renderer.domElement);
         const controls = new OrbitControls(camere, renderer.domElement);
-        controls.target = new THREE.Vector3(3,3,3)
+        // controls.target = new THREE.Vector3(3,3,3)
         controls.enableDamping = true;
+        // texturas
+        const textureLoader = new THREE.TextureLoader();
+        const map = textureLoader.load("./texturas/basecolor.jpg");
+        const aoMap = textureLoader.load("./texturas/ao.jpg");
+        const rounghnessMap = textureLoader.load("./texturas/roughness.jpg");
+        const normalMap = textureLoader.load("./texturas/normal.jpg");
+        const heightMap = textureLoader.load("./texturas/height.png");
         //CUBO
-        const cube = new THREE.Mesh(
-            new THREE.BoxBufferGeometry(1, 1, 1),
-            new THREE.MeshBasicMaterial({ color: 0xff0000, transparent : true, opacity : 0.3, wireframe : true })
-        );
+        const geometry = new THREE.BoxBufferGeometry(1, 1, 1, 250, 250, 250);
+        const material = new THREE.MeshStandardMaterial({
+            map: map,
+            aoMap: aoMap,
+            roughnessMap: rounghnessMap,
+            normalMap: normalMap,
+            displacementMap: heightMap,
+            // wireframe : true
+            displacementScale : 0.07
+        });
+        // const test = new THREE.MeshBasicMaterial({ color: 0xff0000, transparent : true, opacity : 0.3, wireframe : true })
+        const cube = new THREE.Mesh(geometry, material);
+        cube.scale.set(2, 2, 2);
         scene.add(cube);
+        // luces ambiants
+        const AO = new THREE.AmbientLight(0xffffff, 1);
+        scene.add(AO);
 
         // sheper
-        const geometry = new THREE.SphereGeometry(0.8, 32, 16);
-        const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-        const sphere = new THREE.Mesh(geometry, material);
-        scene.add(sphere);
-        sphere.position.x = 2;
-        sphere.position.y = 2;
+        // const geometry = new THREE.SphereGeometry(0.8, 32, 16);
+        // const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+        // const sphere = new THREE.Mesh(geometry, material);
+        // scene.add(sphere);
+        // sphere.position.x = 2;
+        // sphere.position.y = 2;
 
         // torus
 
-        const geometry1 = new THREE.TorusKnotGeometry(0.3, 0.1, 100, 16);
-        const material1 = new THREE.MeshNormalMaterial({flatShading:true});
-        const torusKnot = new THREE.Mesh(geometry1, material1);
-        scene.add(torusKnot);
+        // const geometry1 = new THREE.TorusKnotGeometry(0.3, 0.1, 100, 16);
+        // const material1 = new THREE.MeshNormalMaterial({flatShading:true});
+        // const torusKnot = new THREE.Mesh(geometry1, material1);
+        // scene.add(torusKnot);
 
         // torusKnot.position.x = -2
         // torusKnot.position.y = -0.5
-        torusKnot.position.set(-2, 0.5, 0);
+        // torusKnot.position.set(-2, 0.5, 0);
         // torusKnot.scale.x = 2
         // torusKnot.scale.y = 2
-        torusKnot.scale.set(2, 2, 1);
+        // torusKnot.scale.set(2, 2, 1);
         //render the scene
-        
+
         const animate = () => {
-            controls.update()
+            controls.update();
             renderer.render(scene, camere);
-            requestAnimationFrame(animate)
-
-        }
-        animate()
+            requestAnimationFrame(animate);
+        };
+        animate();
         return () => {
-
             currentMount.removeChild(renderer.domElement);
         };
     }, []);
